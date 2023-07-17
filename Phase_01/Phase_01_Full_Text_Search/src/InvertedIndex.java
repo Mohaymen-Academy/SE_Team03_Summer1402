28f45e23b4f07118db9a563607a8fd8c3c88d67b
Ali Athary <ali.athary.ce@gmail.com>
2023-07-17 09:55:18 +0330

    sreach with + and -

--- Phase_01/Phase_01_Full_Text_Search/src/InvertedIndex.java
+++ Phase_01/Phase_01_Full_Text_Search/src/InvertedIndex.java
@@ -1,29 +1,37 @@
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class InvertedIndex {
-    private Hashtable<String, ArrayList<Integer>> inverted_map;
+    private Hashtable<String, Set<Integer>> inverted_map;
     private ArrayList<Set<String>> books;
 
     public  InvertedIndex(ArrayList<Set<String>> books){
         this.books = books;
-        this.inverted_map = new Hashtable<String, ArrayList<Integer>>();
+        this.inverted_map = new Hashtable<String, Set<Integer>>();
         CalculateIndexes();
     }
 
     private void CalculateIndexes(){
-        for (int i = 0; i < books.stream().count(); i++){
+        for (int i = 0; i < books.size(); i++){
             for (String word : books.get(i)){
                 if(word.equals("")){
                     continue;
                 }
                 if(!inverted_map.containsKey(word)){
-                    inverted_map.put(word, new ArrayList<Integer>());
+                    inverted_map.put(word, new HashSet<Integer>());
                 }
                 inverted_map.get(word).add(i);
             }
         }
+    }
+
+    public Set<Integer> GetDocumentSet(String word){
+        if (inverted_map.get(word) != null){
+            return inverted_map.get(word);
+        }
+        return new HashSet<Integer>();
     }
 
     public void PrintOutput(){
