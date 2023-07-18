d3453be1822f813ce0718ee38c7a2e6b9b6e4383
Ali Athary <ali.athary.ce@gmail.com>
2023-07-18 15:15:43 +0330

    final comments completed.

--- Phase_01/Phase_01_Full_Text_Search/src/Main.java
+++ Phase_01/Phase_01_Full_Text_Search/src/Main.java
@@ -3,23 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+    /**
+     * To use the full text search library, first you should construct
+     * an instance of FullTextSearch class and give the path to your documents'
+     * folder and normalization method and tokenizer. then you can search by
+     * calling the Search method on your FullTextSearch instance.
+     */
     public static void main(String[] args) throws IOException {
+        // construct the FullTextSearch instance with documents' path
         FullTextSearch fts = new FullTextSearch("..\\Books",
                 new RemoveMarksAndUpperCaseNormalization(),
                 new StringTokenizer(" "));
 
         System.out.println("if you want to stop the program enter empty");
         while (true) {
+            // get search input from the user
             System.out.println("Input your search phrase:");
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(System.in));
             String input = reader.readLine();
 
+            // check to terminate the program
             if(input.strip().equals("")){
                 break;
             }
 
+            // call the search method and check for exception
             String[] searchResult;
             try {
                 searchResult = fts.Search(input);
@@ -28,6 +37,7 @@ public class Main {
                 continue;
             }
 
+            // out put the result
             int i = 1;
             for (String s : searchResult){
                 System.out.println(i + ". " + s);
--- Phase_01/Phase_01_Full_Text_Search/src/Tokenizer.java
+++ Phase_01/Phase_01_Full_Text_Search/src/Tokenizer.java
@@ -1,16 +1,35 @@
+/**
+ * Tokenizer interface.
+ */
 public interface Tokenizer {
 
     String[] Tokenize(String inputString);
 
 }
 
+/**
+ * Tokenizing by spiriting by an input string.
+ */
 class StringTokenizer implements Tokenizer{
 
+    /**
+     * Separator string.
+     */
     private final String separator;
 
+    /**
+     * Constructs the tokenizer with custom separator.
+     * @param separator   the separator string.
+     */
     public StringTokenizer(String separator){
         this.separator = separator;
     }
+
+    /**
+     * Splits the input by the separator string.
+     * @param inputString   input string to tokenize.
+     * @return return the tokenized strings.
+     */
     public String[] Tokenize(String inputString){
         return inputString.split(separator);
     }
