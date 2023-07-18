c3b956fea3fd8ed5684cf157a93b2491e2c0c5ba
Hossein <hossein.ghermezcheshme@gmail.com>
2023-07-18 10:16:13 +0330

    Clean FileReader not completed

--- Phase_01/Phase_01_Full_Text_Search/src/FileReader.java
+++ Phase_01/Phase_01_Full_Text_Search/src/FileReader.java
@@ -2,11 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class FileReader {
-    public static List<String> lastReadNames;
-    public static ArrayList<Set<String>> ReadBooks(String path)
-    {
-        lastReadNames = new ArrayList<String>();
-        ArrayList<Set<String>> books = new ArrayList<Set<String>>();
+    public ArrayList<Set<String>> documents;
+    public String[] documentsName;
+    private List<File> files;
+
+    public FileReader(String folderPath, Normalization normalization, String extention) throws FileNotFoundException {
+        files = new ArrayList<File>();
+        GetFiles(folderPath, extention);
+        documents = new ArrayList<Set<String>>();
+        documentsName = new String[files.size()];
+        GetNames();
+        ReadDocuments(folderPath, normalization, extention);
+    }
+    private void GetFiles(String folderPath, String extention) {
+        File folder = new File(folderPath);
+        File[] folderFiles = folder.listFiles();
+        for(File file : folderFiles) {
+            String name = file.getName();
+            if(name.length() < extention.length() + 1 ||
+                    !name.substring(name.length() - extention.length()).equals(extention)
+                    || name.charAt(name.length() - extention.length() - 1) != '.'){
+                continue;
+            }
+            files.add(file);
+        }
+    }
+    private void GetNames(){
+        for(int i = 0; i < files.size(); i++){
+            String name = files.get(i).getName();
+            documentsName[i] = name.substring(0, name.length() - );
+        }
+    }
+    private void ReadDocuments(String path, Normalization normalization, String extention) throws FileNotFoundException {
         File folder = new File(path);
         File[] files = folder.listFiles();
         Scanner sc = null;
@@ -16,16 +43,12 @@ public class FileReader {
             if(name.length() < 4 || !name.substring(name.length() - 4).equals(".txt")){
                 continue;
             }
-            try {
-                sc = new Scanner(file);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            lastReadNames.add(name.substring(0, name.length() - 4));
+            sc = new Scanner(file);
+            documentsName.add(name.substring(0, name.length() - 4));
             Set<String> book = new HashSet<String>();
             while(sc.hasNextLine()){
                 for(String word : sc.nextLine().strip().split(" ")) {
-                    for(String w : Normalize.Normalized(word)){
+                    for(String w : normalization.Normalize(word)){
                         if(Stop_Words.words.contains(w))
                             continue;
                         book.add(w.toLowerCase());
@@ -33,8 +56,7 @@ public class FileReader {
 
                 }
             }
-            books.add(book);
+            documents.add(book);
         }
-        return books;
     }
 }
