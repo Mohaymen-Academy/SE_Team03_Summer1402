513880836b7835025d8a5cbb3b3107c60e67d420
Ali Athary <ali.athary.ce@gmail.com>
2023-07-18 12:26:05 +0330

    intersection added to set logic

--- Phase_01/Phase_01_Full_Text_Search/src/SetLogic.java
+++ Phase_01/Phase_01_Full_Text_Search/src/SetLogic.java
@@ -14,7 +14,24 @@ class SetLogic{
         return result;
     }
 
-    public static Set<Integer> subtract(Set<Integer> set1, Set<Integer> set2) {
+    public static Set<Integer> Intersection(ArrayList<Set<Integer>> sets){
+        Set<Integer> result = new HashSet<Integer>();
+        for (int num : sets.get(0)){
+            boolean allSetsContainNumber = true;
+            for (int i = 1; i < sets.size(); i++) {
+                if(!sets.get(i).contains(num)){
+                    allSetsContainNumber = false;
+                    break;
+                }
+            }
+            if(allSetsContainNumber){
+                result.add(num);
+            }
+        }
+        return result;
+    }
+
+    public static Set<Integer> Subtract(Set<Integer> set1, Set<Integer> set2) {
         Set<Integer> result = new HashSet<Integer>();
         for(int num : set1){
             if(!set2.contains(num))
