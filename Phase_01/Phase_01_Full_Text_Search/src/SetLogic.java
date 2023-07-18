c89aa55cfe0648861de9833c1503aaf17bb2b6f0
Hossein <hossein.ghermezcheshme@gmail.com>
2023-07-18 12:24:29 +0330

    Union and subtract in SetLogic

--- Phase_01/Phase_01_Full_Text_Search/src/SetLogic.java
+++ Phase_01/Phase_01_Full_Text_Search/src/SetLogic.java
@@ -1,3 +1,25 @@
-class SetLogic{
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+class SetLogic{
+    public static Set<Integer> Union(ArrayList<Set<Integer>> sets){
+        Set<Integer> result = new HashSet<Integer>();
+        for(int i = 0; i < sets.size(); i++)
+        {
+            for(int num : sets.get(i)){
+               result.add(num);
+            }
+        }
+        return result;
+    }
+
+    public static Set<Integer> subtract(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<Integer>();
+        for(int num : set1){
+            if(!set2.contains(num))
+                result.add(num);
+        }
+        return result;
+    }
 }
