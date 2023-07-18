bfe2fae105044741bb6a7e9069a31847523a9275
Hossein <hossein.ghermezcheshme@gmail.com>
2023-07-18 10:59:46 +0330

    Write comment for FileReader and Stop_Words

--- Phase_01/Phase_01_Full_Text_Search/src/FileReader.java
+++ Phase_01/Phase_01_Full_Text_Search/src/FileReader.java
@@ -2,10 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class FileReader {
+
+    /**
+     * Document contents.
+     */
     public ArrayList<Set<String>> documents;
+
+    /**
+     * Document name.
+     */
     public String[] documentsName;
+
+    /**
+     * Document file.
+     */
     private List<File> files;
 
+    /**
+     * Constructs file reader.
+     * @param folderPath   path of documents' folder.
+     * @param normalization   normalization method.
+     * @param extension   documents' file extension.
+     * @throws FileNotFoundException   if path doesn't exist.
+     */
     public FileReader(String folderPath, Normalization normalization, String extension) throws FileNotFoundException {
         files = new ArrayList<File>();
         GetFiles(folderPath, extension);
@@ -14,11 +33,23 @@ public class FileReader {
         documents = new ArrayList<Set<String>>();
         ReadDocuments(folderPath, normalization, extension);
     }
+
+    /**
+     * Gets extension of files.
+     * @param file   file of document.
+     * @return   string of extension.
+     */
     private String GetExtention(File file){
         String name = file.getName();
         String[] splited = name.split("\\.");
         return splited[splited.length - 1];
     }
+
+    /**
+     * Finds all the documents in the folder (with the extension).
+     * @param folderPath   path of documents' folder.
+     * @param extention   documents' file extension.
+     */
     private void GetFiles(String folderPath, String extention) {
         File folder = new File(folderPath);
         File[] folderFiles = folder.listFiles();
@@ -30,12 +61,24 @@ public class FileReader {
             files.add(file);
         }
     }
+
+    /**
+     * Stores name of document without extension.
+     */
     private void GetNames(){
         for(int i = 0; i < files.size(); i++){
             String name = files.get(i).getName();
             documentsName[i] = name.substring(0, name.length() - GetExtention(files.get(i)).length());
         }
     }
+
+    /**
+     * Reads documents' content and stores them.
+     * @param path   path of documents' folder.
+     * @param normalization   normalization method.
+     * @param extention   documents' file extension.
+     * @throws FileNotFoundException   if path doesn't exist.
+     */
     private void ReadDocuments(String path, Normalization normalization, String extention) throws FileNotFoundException {
         Scanner sc = null;
         StringBuffer sd = new StringBuffer();
