1d733255408d5b47957306b122d7cf7b55431ec0
Ali Athary <ali.athary.ce@gmail.com>
2023-07-25 12:01:46 +0330

    NormalizationTest and TokenizationTest test units.

---
+++ Phase_01/Phase_01_Full_Text_Search/src/main/java/unit_test/TokenizationTest.java
@@ -0,0 +1,52 @@
+package unit_test;
+
+import word_manipulation.tokenization.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class TokenizationTest {
+
+    @Test
+    public void tokenize_StringTokenizerWhiteSpace(){
+        Tokenizer tokenizer = new StringTokenizer(" ");
+        String[] actual = tokenizer.tokenize("a bc !d $");
+        String[] expected = {"a", "bc", "!d", "$"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenize_StringTokenizerLongSeparator(){
+        Tokenizer tokenizer = new StringTokenizer("_ ");
+        String[] actual = tokenizer.tokenize("a_ bc !d_ _e$");
+        String[] expected = {"a", "bc !d", "_e$"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenize_MarksTokenizer(){
+        Tokenizer tokenizer = new MarksTokenizer();
+        String[] actual = tokenizer.tokenize("a_ bc !d_ _e$");
+        String[] expected = {"a", "bc", "d", "e"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void tokenize_EdgeNgramTokenizer(){
+        Tokenizer tokenizer = new EdgeNgramTokenizer(2, 3);
+        String[] actual = tokenizer.tokenize("AbCd_ _e123$");
+        Arrays.sort(actual);
+        String[] expected = {
+                "Ab", "bC", "Cd",
+                "AbC", "bCd",
+                "AbCd",
+                "e1", "12", "23",
+                "e12", "123",
+                "e123"};
+        Arrays.sort(expected);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
