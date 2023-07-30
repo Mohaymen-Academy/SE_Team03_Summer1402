697844a8667717d85b5d14d817c931af62759737
Hossein <hossein.ghermezcheshme@gmail.com>
2023-07-30 12:57:13 +0330

    create sequence for each table

--- Messenger/SQL_Queries/Creat_Tables.sql
+++ Messenger/SQL_Queries/Creat_Tables.sql
@@ -1,15 +1,29 @@
 DROP TABLE IF EXISTS Chat_Member;
 DROP TABLE IF EXISTS Chat_Message;
+DROP SEQUENCE IF EXISTS Chat_Message_id_seq;
 DROP TABLE IF EXISTS Chat;
+DROP SEQUENCE IF EXISTS Chat_id_seq;
 DROP TABLE IF EXISTS App_User;
 DROP SEQUENCE IF EXISTS App_User_id_seq;
 DROP TABLE IF EXISTS Image;
+DROP SEQUENCE IF EXISTS Image_id_seq;
+
+
+-- IMAGE TABLE:
+
+CREATE SEQUENCE Image_id_seq;
 
 CREATE TABLE Image
 (
-	image_id		INT				PRIMARY KEY,
+	image_id		INT				PRIMARY KEY		DEFAULT nextval('Image_id_seq'),
 	image_data		OID				NOT NULL
 );
+
+ALTER SEQUENCE Image_id_seq
+OWNED BY Image.image_id;
+
+
+-- APP_USER TABLE:
 
 CREATE SEQUENCE App_User_id_seq;
 
@@ -26,16 +40,29 @@ CREATE TABLE App_User
 ALTER SEQUENCE App_User_id_seq
 OWNED BY App_User.user_id;
 
+
+-- CHAT TABLE:
+
+CREATE SEQUENCE Chat_id_seq;
+
 CREATE TABLE Chat
 (
-	chat_id 		INT				PRIMARY KEY,
+	chat_id 		INT				PRIMARY KEY		DEFAULT nextval('Chat_id_seq'),
 	chat_type		SMALLINT		NOT NULL,
 	chat_name		VARCHAR(50)
 );
 
+ALTER SEQUENCE Chat_id_seq
+OWNED BY Chat.chat_id;
+
+
+-- CHAT_MESSAGE TABLE:
+
+CREATE SEQUENCE Chat_Message_id_seq;
+
 CREATE TABLE Chat_Message
 (
-	message_id		INT				PRIMARY KEY,
+	message_id		INT				PRIMARY KEY		DEFAULT nextval('Chat_Message_id_seq'),
 	seen 			BOOL			DEFAULT 'false',
 	time_message	DATE			NOT NULL,
 	have_text		BOOL			DEFAULT 'false',
@@ -52,6 +79,12 @@ CREATE TABLE Chat_Message
 	FOREIGN KEY(fk_chat_id) REFERENCES Chat(chat_id),
 	FOREIGN KEY(fk_user_id) REFERENCES App_User(user_id)
 );
+
+ALTER SEQUENCE Chat_Message_id_seq
+OWNED BY Chat_Message.message_id;
+
+
+-- CHAT_MEMBER TABLE:
 
 CREATE TABLE Chat_Member
 (
